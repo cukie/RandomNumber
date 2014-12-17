7473780304a3271049fc09fd104d08c512ee3c60
cukie <gil.cukierman@gmail.com>
2014-12-17 13:34:49 -0500

    initial commit

---
+++ RandomNumber/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  RandomNumber
+//
+//  Created by Gil Cukierman on 12/17/14.
+//  Copyright (c) 2014 Gil Cukierman. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ViewController: UIViewController, UITextFieldDelegate  {
+  
+  
+  @IBOutlet weak var displayNumber: UILabel!
+  @IBOutlet weak var upperBoundField: UITextField!
+  
+  @IBAction func randomizeNumber(sender: AnyObject) {
+  
+    // when the button is pressed, hide keyboard
+    
+    let string = upperBoundField.text
+    let num = string.toInt()
+    let unum = UInt32(num!)
+    let rand = arc4random_uniform(unum)
+    
+    let toDisplay = String(rand)
+    self.displayNumber.text = toDisplay
+    
+    
+  }
+  
+  
+  override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+    self.view.endEditing(true)
+  }
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
